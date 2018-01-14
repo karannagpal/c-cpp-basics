0a5892e0da18466d95421f00301deeaa018d46f0
Karan Nagpal <karannagpal96@gmail.com>
2018-01-14 19:23:55 +0530

    problem solved for overflow

--- Level 2/2. UCA Questions/uca 101.c
+++ Level 2/2. UCA Questions/uca 101.c
@@ -3,11 +3,10 @@
 int main()
 {
     int n, b = 0, x, pow = 1, carry = 0;
-    //scanf("%d", &n);
-    n = 1234;
+    scanf("%d", &n);
     while(n != 0)
     {
-        b = b + carry;
+        b = b + (carry * pow);
         x = n % 10;
         if(x == 9)
         {
@@ -22,6 +21,12 @@ int main()
         b = b + (x*pow);
         n = n/10;
         pow = pow * 10;
+
+        if(n == 0 && carry == 1)
+        {
+            b = b + (carry * pow);
+        }
+
     }
     printf("%d", b);
     return 0;
